02891df5cd2a79540987b2f18dbed7dabdfdbab3
Guadalupe Vazquez <guvadlt@gmail.com>
2018-11-15 22:17:01 -0600

    Se importo lo necesario en PruebaDIJKSTRA.java

--- src/PruebaDIJKSTRA.java
+++ src/PruebaDIJKSTRA.java
@@ -1,3 +1,11 @@
+//http://jorgep.blogspot.com/2010/10/ruta-mas-corta-solucion-por-el.html
+import java.util.LinkedList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.Stack;
+import java.util.Vector;
 
 public class PruebaDIJKSTRA {
 
